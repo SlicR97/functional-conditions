8776a1b2b2ffe735dea400a3cdead79b2ebf6b60
Nico Loos <nico.loos@slicr.de>
2022-05-20 13:51:40 +0100

    Optional Globals

--- src/if.ts
+++ src/if.ts
@@ -1,32 +1,41 @@
 /**
- * Function type for then / else branches of $if, 
- * in case more complex operations need to take place 
- * in these expressions.
- * 
- * @param Type Return type
- */
- type IfFunction<Type> = () => Type;
+* Function type for then / else branches of $if, 
+* in case more complex operations need to take place 
+* in these expressions.
+* 
+* @param Type Return type
+*/
+type IfFunction<Type> = () => Type;
 
- /**
-  * Functional @see if expression
-  * 
-  * @param condition Condition to be evaluated
-  * @param thenCase Case that is returned if condition is truthy
-  * @param elseCase Case that is returned if condition is falsy
-  * @returns Either thenCase or elseCase, or the result of their operations, respectively
-  */
- export const $if = <TThen, TElse>(
-   condition: any,
-   thenCase: IfFunction<TThen> | TThen,
-   elseCase: IfFunction<TElse> | TElse,
- ): TThen | TElse => {
-   const $return = condition
-     ? thenCase
-     : elseCase;
- 
-   if (typeof $return === 'function') {
-     return ($return as IfFunction<TThen | TElse>)();
-   } else {
-     return $return;
-   }
- };
+/**
+ * Function type provider for globalize.ts
+ */
+export type IfFunctionType = <TThen, TElse>(
+  condition: any,
+  thenCase: IfFunction<TThen> | TThen,
+  elseCase: IfFunction<TElse> | TElse
+) => TThen | TElse;
+
+/**
+ * Functional @see if expression
+ * 
+ * @param condition Condition to be evaluated
+ * @param thenCase Case that is returned if condition is truthy
+ * @param elseCase Case that is returned if condition is falsy
+ * @returns Either thenCase or elseCase, or the result of their operations, respectively
+ */
+export const $if = <TThen, TElse>(
+  condition: any,
+  thenCase: IfFunction<TThen> | TThen,
+  elseCase: IfFunction<TElse> | TElse,
+): TThen | TElse => {
+  const $return = condition
+    ? thenCase
+    : elseCase;
+
+  if (typeof $return === 'function') {
+    return ($return as IfFunction<TThen | TElse>)();
+  } else {
+    return $return;
+  }
+};
---
+++ test/globalize.test.ts
@@ -0,0 +1,26 @@
+import { equal } from 'assert';
+import '../src/globalize';
+
+describe('globalize.ts', () => {
+  it('should provide $if as a global function', () => {
+    const actual = $if(
+      true,
+      1,
+      2
+    );
+
+    equal(actual, 1);
+  });
+
+  it('should provide $switch as a global function', () => {
+    const actual = $switch({
+      with: 'a',
+      cases: [
+        ['a', 1]
+      ],
+      default: 2
+    });
+
+    equal(actual, 1);
+  });
+});
